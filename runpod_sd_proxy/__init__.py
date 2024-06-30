5f20a0e41f13d9f465b0a390497a7de42dff8de9
Tyler F. Carr <tcarwash@gmail.com>
2024-06-30 11:49:06 -0700

    implement runpod sdxl

--- runpod_sd_proxy/__init__.py
+++ runpod_sd_proxy/__init__.py
@@ -1,1 +1,27 @@
+from flask import Flask
+import sqlite3
+import logging
+import sys
+import os
+
+logger = logging.getLogger(__name__)
+if os.environ.get("DEBUG"):
+    logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+app = Flask(__name__)
+db = sqlite3.connect("/data/runpod_sd_proxy.db", check_same_thread=False)
+cur = db.cursor()
+cur.execute("DROP TABLE IF EXISTS model;")
+cur.execute(
+    """
+    CREATE TABLE model (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    use_model TEXT NOT NULL DEFAULT 'v1-5-pruned-emaonly'
+    );
+    """
+)
+test = cur.execute("INSERT INTO model (use_model) VALUES ('v1-5-pruned-emaonly');")
+use_model = cur.lastrowid
+
 from .app import app
--- runpod_sd_proxy/app.py
+++ runpod_sd_proxy/app.py
@@ -1,35 +1,57 @@
-from flask import Flask, request, jsonify
+from flask import request, jsonify
+from runpod_sd_proxy import app, use_model, cur, logger
 import requests
 import os
 
 RUNPOD_API_KEY = os.environ.get("RUNPOD_API_KEY")
 RUNPOD_BASE_URL = os.environ.get("RUNPOD_BASE_URL")
-
-app = Flask(__name__)
-
-
-@app.route("/sdapi/v1/sd-models")
-def models():
-    """returns models"""
-    response = [
+RUNPOD_BASE_URL_SDXL = os.environ.get("RUNPOD_BASE_URL_SDXL")
+MODELS = []
+if RUNPOD_BASE_URL is not None:
+    MODELS = [
         {
             "title": "v1-5",
             "model_name": "v1-5-pruned-emaonly",
         }
     ]
-    return jsonify(response)
+if RUNPOD_BASE_URL_SDXL is not None:
+    MODELS.append(
+        {
+            "title": "v1-5-sdxl",
+            "model_name": "v1-5-sdxl",
+        }
+    )
+
+USE_MODEL = "v1-5-pruned-emaonly"
+
+
+@app.route("/sdapi/v1/sd-models")
+def models():
+    """returns models"""
+    if MODELS:
+        logger.debug(MODELS)
+        return jsonify(MODELS)
+    else:
+        return jsonify({"error": "No models available"})
 
 
 @app.route("/sdapi/v1/options")
 def options():
     """returns model options"""
-    return jsonify({"sd_model_checkpoint": "v1-5-pruned-emaonly"})
+    model = cur.execute(
+        f"SELECT use_model FROM model WHERE id = {use_model};"
+    ).fetchone()[0]
+    logger.debug(model)
+    return jsonify({"sd_model_checkpoint": model})
 
 
 @app.route("/sdapi/v1/options", methods=["POST"])
 def options_post():
     """returns image"""
     model = request.json.get("sd_model_checkpoint")
+    if model not in [m["model_name"] for m in MODELS]:
+        return jsonify({"error": "Invalid model"})
+    cur.execute(f"UPDATE model SET use_model = '{model}' WHERE id = {use_model};")
     return jsonify({"sd_model_checkpoint": model})
 
 
@@ -40,14 +62,32 @@ def generate_image():
     sd_request = {"input": {"prompt": prompt}}
 
     headers = {"Authorization": f"Bearer {RUNPOD_API_KEY}"}
+    logger.debug(request.json)
+    model = cur.execute(
+        f"SELECT use_model FROM model WHERE id = {use_model};"
+    ).fetchone()[0]
 
-    sd_response = requests.post(
-        RUNPOD_BASE_URL,
-        headers=headers,
-        json=sd_request,
-    )
-    sd_response_json = sd_response.json()
-    image = sd_response_json.get("output", {}).get("images", [])[0]
+    if model == "v1-5-sdxl" and RUNPOD_BASE_URL_SDXL is not None:
+        logger.debug("using sdxl")
+        sd_response = requests.post(
+            RUNPOD_BASE_URL_SDXL,
+            headers=headers,
+            json=sd_request,
+            timeout=60,
+        )
+        sd_response_json = sd_response.json()
+        image = sd_response_json.get("output", {}).get("image_url", "")
+        logger.debug(image)
+        image = image.replace("data:image/png;base64,", "")
+    elif model == "v1-5-pruned-emaonly":
+        logger.debug("using pruned")
+        sd_response = requests.post(
+            RUNPOD_BASE_URL,
+            headers=headers,
+            json=sd_request,
+        )
+        sd_response_json = sd_response.json()
+        image = sd_response_json.get("output", {}).get("images", [])[0]
     response = {"images": [image], "parameters": {}, "info": prompt}
     return jsonify(response)
 
